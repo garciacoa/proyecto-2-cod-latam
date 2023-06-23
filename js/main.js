0b95f281188ff74f39088254aa09e3e0ccdf81ca
repooagc <garciacoa@gmail.com>
2023-06-22 18:53:03 -0600

    Commit Trabajo Final

--- js/main.js
+++ js/main.js
@@ -5,10 +5,27 @@ menuIcon.addEventListener('click', function () {
     menu.classList.toggle('active');
 });
 
+window.addEventListener('load', iniciar, false);
+
+function iniciar() {
+  var imagen = document.getElementById('imagen');
+  imagen.addEventListener('mouseover', peligro, false);
+  imagen.addEventListener('mouseout', restaurar, false);
+}
+
+function peligro() {
+  var imagen = document.getElementById('imagen').src = "img/Oscar2.jpg";
+}
+
+   function restaurar() {
+    var imagen = document.getElementById('imagen').src = "img/Oscarc.png";
+  }
 
 
 
+const darkmodebt = document.getElementById('darkmodebt')
+const body = document.body;
 
-
-
-
+darkmodebt.addEventListener('click', function () {
+body.classList.toggle ('dark-mode');
+});
